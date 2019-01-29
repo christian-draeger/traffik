22ce9adf479a431363f0868b70fa82e7f462c290
Christian Dräger <christian.draeger1@gmail.com>
2019-01-29 17:00:45 +0100

    can send messages via stomp/websockets from broker to client

--- src/main/kotlin/it/skrape/traffik/ui/MessageController.kt
+++ src/main/kotlin/it/skrape/traffik/ui/MessageController.kt
@@ -3,13 +3,14 @@ package it.skrape.traffik.ui
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class MessageController {
+class MessageController(val template: SimpMessagingTemplate) {
 
     val messageRepository = mutableListOf(mapOf("status" to "OK"), mapOf("status" to "ERROR"))
 
@@ -28,5 +29,11 @@ class MessageController {
     @GetMapping("/add")
     fun add(@RequestParam status: String) {
         messageRepository.add(mapOf("status" to status))
+        template.convertAndSend("/topic/all", messageRepository)
+    }
+
+    fun sendMessage(msg: Map<String, String>) {
+        messageRepository.add(msg)
+        template.convertAndSend("/topic/all", messageRepository)
     }
 }
